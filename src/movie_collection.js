b04031826270da1a7c8862a9edef4995c9d09d9a
KimDoHyun1011 <hot99009@gmail.com>
2024-07-31 20:02:21 +0900

    edit : css 위치수정 작업

--- src/movie_collection.js
+++ src/movie_collection.js
@@ -5,8 +5,8 @@ const options = {
     accept: "application/json",
     Authorization:
       "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ODVhN2QxNzMwNDgxMTM4MTNkZTkwNGRmOWYzNGE3ZiIsIm5iZiI6MTcyMTg2NzkxNS4wNjczNywic3ViIjoiNjVmZDRjYWQ3NzA3MDAwMTdjMGE4MTZmIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.cbkZQGyQg8TkXQEW93ZyqtaR4dVrGhw2j-1FJPufuus",
-  },
-};
+  }
+}
 
 // fetch(
 //   "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
